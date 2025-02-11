1911b0cf112058603cea5e8d72207f222df56ba7
Ryan Silva <ryansamuel432@gmail.com>
2025-02-11 20:43:41 -0300

    Base url added

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,11 +5,11 @@ describe('Swag Labs Tests', () => {
     passwordField: '[name="password"]',
     loginButton: '[type="submit"]',
     sectionTitleTopBar: '.product_label',
-    wrongCrefentialAlert: '[data-test="error"]',
+    wrongCredentialAlert: '[data-test="error"]',
   }
 
   it('Login valido', () => {
-    cy.visit('https://www.saucedemo.com/v1/index.html')
+    cy.visit('/v1/index.html')
     cy.get(selectorsList.usernameField).type('standard_user')
     cy.get(selectorsList.passwordField).type('secret_sauce')
     cy.get(selectorsList.loginButton).click()
@@ -17,8 +17,8 @@ describe('Swag Labs Tests', () => {
     cy.get(selectorsList.sectionTitleTopBar).contains('Products')
   })
 
-  it.only('Fazendo compras', () => {
-    cy.visit('https://www.saucedemo.com/v1/inventory.html')
+  it('Fazendo compras', () => {
+    cy.visit('/v1/inventory.html')
     cy.get(':nth-child(1) > .pricebar > .btn_primary').click()
     cy.get(':nth-child(2) > .pricebar > .btn_primary').click()
     cy.get(':nth-child(4) > .pricebar > .btn_primary').click()
@@ -35,10 +35,10 @@ describe('Swag Labs Tests', () => {
   })
 
   it('Login invalido', () => {  //"it.only" é usado para rodar somente este teste" e it.skip é usado para pular
-    cy.visit('https://www.saucedemo.com/v1/index.html')
+    cy.visit('/v1/index.html')
     cy.get('[data-test="username"]').type('standard_use')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('#login-button').click()
-    cy.get(selectorsList.wrongCrefentialAlert)
+    cy.get(selectorsList.wrongCredentialAlert)
   })
 })
